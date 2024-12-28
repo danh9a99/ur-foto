70781388cf018b35131281dffe7b690b35fa2ab2
Dan Ng <ngothanhdanhtqt@gmail.com>
2024-12-28 15:34:54 +0700

    update-component

--- fotowebapp/urls.py
+++ fotowebapp/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("galary/", views.galary, name="galary"),
+    path("login/", views.login, name="login"),
+    path("register/", views.register, name="register"),
+    path("blog/", views.blog, name="blog"),
 ]
--- fotowebapp/views.py
+++ fotowebapp/views.py
@@ -1,8 +1,27 @@
 from django.http import HttpResponse
 from django.template import loader
 
-def index(request): 
+def index(request):
     template_name = "fotowebapp/index.html"
     template = loader.get_template(template_name)
-    # content = loader.render_to_string(template_name, context)
+    return HttpResponse(template.render())
+
+def galary(request): 
+    template_name = "fotowebapp/galary.html"
+    template = loader.get_template(template_name)
+    return HttpResponse(template.render())
+
+def login(request):
+    template_name = "fotowebapp/login.html"
+    template = loader.get_template(template_name)
+    return HttpResponse(template.render())
+
+def register(request):
+    template_name = "fotowebapp/register.html"
+    template = loader.get_template(template_name)
+    return HttpResponse(template.render())
+
+def blog(request):
+    template_name = "fotowebapp/blog.html"
+    template = loader.get_template(template_name)
     return HttpResponse(template.render())
